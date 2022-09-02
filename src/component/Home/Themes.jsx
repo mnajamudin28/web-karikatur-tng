276c30ecfb6befc934299a31c5dbb790191de261
unknown <m.najamudin@gmail.com>
2022-09-02 17:03:46 +0700

    navigation

--- src/component/Home/Themes.jsx
+++ src/component/Home/Themes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, scrollbar } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 import graduation from '../../img/graduation.png'
 import family from '../../img/family.png'
 import group from '../../img/group.png'
 import kids from '../../img/kids.png'
 import wedding from '../../img/wedding.png'
+import friends from '../../img/friends.png'
+import uniform from '../../img/uniform.png'
 
 const Themes = () => {
   return (
@@ -12,21 +19,35 @@ const Themes = () => {
          <hr />
          <h1>Themes</h1>
          <p>This is the result of the caricature order to us so far.</p>
+
          <div className='themes-img'>
-              <a href="#"><img src={graduation} alt="" /></a>
-              <a href="#"><img src={wedding} alt="" /></a>
-              <a href="#"><img src={family} alt="" /></a>
-              <a href="#"><img src={group} alt="" /></a>
-              <a href="#"><img src={kids} alt="" /></a>
-         </div>
-           {/* <div className='themes-img'>
-              <a href="#" className='img-1'>sasas</a>
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
-              <a href="#"></a>
-           </div> */}
-               
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation]}
+            spaceBetween={50}
+            slidesPerView={4}
+            navigation
+            // scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            <SwiperSlide><a href="#"><div className='box-theme'></div><img src={graduation} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={wedding} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={family} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={group} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={kids} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={friends} alt="" /></a></SwiperSlide>
+            <SwiperSlide><a href="#"><img src={uniform} alt="" /></a></SwiperSlide>
+          </Swiper>
+          
+          
+          
+          
+          
+          
+          
+         </div>     
+
       </div>
       
     </div>
